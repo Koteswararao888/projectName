3e1db4770c889231e391457be103339853b37505
Developer <developer@whitepoker.com>
2026-06-02 12:58:09 +0530

    Initial Project Setup

---
+++ src/test/java/com/hds/projectName/ProjectNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hds.projectName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
